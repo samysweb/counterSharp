96009bd44039a5c21442e5a8b90216470f8fc100
Samuel Teuber <samuel@samweb.org>
2020-06-14 11:25:53 +0200

    assert(0) only in checked function (not in subroutines)

--- counterSharp/source/visitors/transforms/ReturnTransformVisitor.py
+++ counterSharp/source/visitors/transforms/ReturnTransformVisitor.py
@@ -10,7 +10,8 @@ class ReturnTransformVisitor(TransformVisitor):
 	def __init__(self, configParam):
 		super().__init__(configParam)
 	def visit_FuncDef(self, node, parents):
-		v = ReturnStmtTransformVisitor(self.config)
+		isCheckedFunction = (node.decl.name==self.config.function)
+		v = ReturnStmtTransformVisitor(self.config, isCheckedFunction)
 		v.visit(node.body,[])
 		if not v.foundReturn:
 			assertStmt = c_ast.FuncCall(
@@ -19,19 +20,21 @@ class ReturnTransformVisitor(TransformVisitor):
 					[c_ast.Constant('int', "0", coord=ReturnTransformVisitor.TransformCoord),c_ast.Constant('string', "\"\"", coord=ReturnTransformVisitor.TransformCoord)]
 				, coord=ReturnTransformVisitor.TransformCoord)
 				, coord=ReturnTransformVisitor.TransformCoord)
-			compoundStmt = c_ast.Compound([
-					assertStmt,
-					c_ast.Return(None, coord=ReturnTransformVisitor.TransformCoord)
-				], coord=ReturnTransformVisitor.TransformCoord)
+			stmtList = []
+			if isCheckedFunction:
+				stmtList.append(assertStmt)
+			stmtList.append(c_ast.Return(None, coord=ReturnTransformVisitor.TransformCoord))
+			compoundStmt = c_ast.Compound(stmtList, coord=ReturnTransformVisitor.TransformCoord)
 			label = c_ast.Label(self.config.returnLabel, compoundStmt,
 				coord=ReturnTransformVisitor.TransformCoord)
 			node.body.block_items.append(label)
 		return None
 
 class ReturnStmtTransformVisitor(TransformVisitor):
-	def __init__(self, config):
+	def __init__(self, config, isCheckedFunction):
 		super().__init__(config)
 		self.foundReturn = False
+		self.isCheckedFunction=isCheckedFunction
 	def visit_Return(self, node, parents):
 		assertStmt = c_ast.FuncCall(
 			c_ast.ID("__CPROVER_assert", coord=ReturnTransformVisitor.TransformCoord),
@@ -39,10 +42,11 @@ class ReturnStmtTransformVisitor(TransformVisitor):
 				[c_ast.Constant('int', "0", coord=ReturnTransformVisitor.TransformCoord),c_ast.Constant('string', "\"\"", coord=ReturnTransformVisitor.TransformCoord)]
 			, coord=ReturnTransformVisitor.TransformCoord)
 			, coord=ReturnTransformVisitor.TransformCoord)
-		compoundStmt = c_ast.Compound([
-				assertStmt,
-				node
-			], coord=ReturnTransformVisitor.TransformCoord)
+		stmtList = []
+		if self.isCheckedFunction:
+			stmtList.append(assertStmt)
+		stmtList.append(node)
+		compoundStmt = c_ast.Compound(stmtList, coord=ReturnTransformVisitor.TransformCoord)
 		if not self.foundReturn:
 			self.foundReturn = True
 			return c_ast.Label(self.config.returnLabel, compoundStmt,
